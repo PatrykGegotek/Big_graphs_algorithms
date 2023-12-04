0e9cee7100165629c48c4b16ce1cf50d9ae0def8
Patryk <patryk26g@gmail.com>
2023-12-04 17:52:03 +0100

    Improvements

--- Algorithms/Pregel.py
+++ Algorithms/Pregel.py
@@ -1,6 +1,8 @@
 import json
 import random
+import time
 from typing import Dict
+import plotly.graph_objects as go
 
 import folium as folium
 import geopandas as gpd
@@ -13,7 +15,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-from Data_Processing import Data_Processing
+from Data import Data_Processing
 from shapely.wkt import loads
 from matplotlib.colors import LinearSegmentedColormap
 
@@ -21,16 +23,24 @@ MAX_WEIGHT = 20
 MESSAGES_NUMBER = 0
 SENT_MESSAGES = 0
 
+
 def initialize_geodataframe(graph):
     rows = []
     for vertex in graph.vertices.values():
         for edge in vertex.edges:
             start_vertex = graph.vertices[edge.start_vertex]
             end_vertex = graph.vertices[edge.end_vertex]
+            edge_id = edge.id
+            oneway = edge.oneway
             line = LineString([(start_vertex.x, start_vertex.y), (end_vertex.x, end_vertex.y)])
-            rows.append({'start': edge.start_vertex, 'end': edge.end_vertex, 'geometry': line, 'traffic': []})
-    print(gpd.GeoDataFrame(rows, columns=['start', 'end', 'geometry', 'traffic']))
-    return gpd.GeoDataFrame(rows, columns=['start', 'end', 'geometry', 'traffic'])
+            rows.append({'start': edge.start_vertex,
+                         'end': edge.end_vertex,
+                         'edge_id': edge_id,
+                         'oneway': oneway,
+                         'geometry': line,
+                         'traffic': []})
+    return gpd.GeoDataFrame(rows, columns=['start', 'end', 'edge_id', 'oneway', 'geometry', 'traffic'])
+
 
 def update_geodataframe(graph, gdf):
     for i, row in gdf.iterrows():
@@ -44,6 +54,45 @@ def update_geodataframe(graph, gdf):
         edge_traffic = (start_vertex.value + end_vertex.value) / 2
         row['traffic'].append(edge_traffic)
     return gdf
+
+
+def process_dataframe(df):
+    rows_to_add = []
+    matched_ids = set()
+
+    for i in range(len(df)):
+        if df.loc[i, 'edge_id'] in matched_ids:
+            continue
+        if df.loc[i, 'oneway']:
+            # rows_to_add.append(df.iloc[i])
+            new_row = {
+                'start': df.loc[i, 'start'],
+                'end': df.loc[i, 'end'],
+                'traffic': df.loc[i, 'traffic'],
+                'geometry': df.loc[i, 'geometry']
+            }
+
+            rows_to_add.append(new_row)
+        else:
+            edge_id = df.loc[i, 'edge_id']
+            duplicate_row = df[(df['edge_id'] == edge_id)]
+
+            if not duplicate_row.empty:
+                duplicate_index = duplicate_row.index[0]
+                summed_traffic = [sum(x) for x in zip(df.loc[i, 'traffic'], df.loc[duplicate_index, 'traffic'])]
+
+                new_row = {
+                    'start': df.loc[i, 'start'],
+                    'end': df.loc[i, 'end'],
+                    'traffic': summed_traffic,
+                    'geometry': df.loc[i, 'geometry']
+                }
+
+                rows_to_add.append(new_row)
+            matched_ids.add(edge_id)
+    new_df = gpd.GeoDataFrame(rows_to_add)
+    print(new_df)
+    return new_df
 
 
 def draw_graph(graph, ax, step):
@@ -92,8 +141,11 @@ class Vertex:
         self.edges.append(edge)
 
     def initialize_traffic(self, probability, max_value, weight):
-        if random.random() < (probability * weight / MAX_WEIGHT):
-            self.value = random.randint(1, max_value * weight)
+        if random.random() < (probability * (weight ** 2) / (MAX_WEIGHT ** 2)):
+            if weight > 10:
+                self.value = random.randint(int(max_value/2), max_value)
+            else:
+                self.value = random.randint(1, int(max_value / 2))
             self.active = True
 
     def compute(self):
@@ -121,7 +173,7 @@ class Vertex:
                     transmit_value = int(random.uniform(0.4, 1.0) * minimal_value * edge.weight)
                     if transmit_value == 0 or transmit_value > self.value:
                         transmit_value = self.value
-                    target_vertex = graph.vertices[edge.end_vertex] # DO EDYCJI
+                    target_vertex = graph.vertices[edge.end_vertex]  # DO EDYCJI
                     target_vertex.messages.append(transmit_value)
                     target_vertex.message_received = True
                     self.value -= transmit_value
@@ -132,11 +184,14 @@ class Vertex:
 
 class Edge:
     static_edges = []
+    edge_id = 0
 
-    def __init__(self, start_vertex, end_vertex, weight):
+    def __init__(self, start_vertex, end_vertex, weight, id, oneway):
         self.start_vertex = start_vertex
         self.end_vertex = end_vertex
         self.weight = weight
+        self.id = id
+        self.oneway = oneway
 
     def __str__(self):
         return f"Edge(from: {self.start_vertex}, to: {self.end_vertex}, weight: {self.weight:.0f})"
@@ -151,20 +206,30 @@ class Graph:
         if vertex.id not in self.vertices:
             self.vertices[vertex.id] = vertex
 
-    def add_edge(self, start_point, end_point, start_id, end_id, weight):
+    def add_edge(self, start_point, end_point, start_id, end_id, oneway, weight):
+        # if start_id not in self.vertices:
+        #     self.add_vertex(Vertex(start_id, start_point.x, start_point.y))
+        # if end_id not in self.vertices:
+        #     self.add_vertex(Vertex(end_id, end_point.x, end_point.y))
+        is_oneway = oneway == 'yes'
         if start_id not in self.vertices:
-            self.add_vertex(Vertex(start_id, start_point.x, start_point.y))
+            self.add_vertex(Vertex(start_id, start_point[0], start_point[1]))
         if end_id not in self.vertices:
-            self.add_vertex(Vertex(end_id, end_point.x, end_point.y))
+            self.add_vertex(Vertex(end_id, end_point[0], end_point[1]))
 
-        edge = Edge(start_id, end_id, weight)
+        edge = Edge(start_id, end_id, weight, Edge.edge_id, is_oneway)
         self.edges.append(edge)
         self.vertices[start_id].add_edge(edge)
+        if not is_oneway:
+            edge = Edge(end_id, start_id, weight, Edge.edge_id, is_oneway)
+            self.edges.append(edge)
+            self.vertices[end_id].add_edge(edge)
+        Edge.edge_id += 1
 
     def initialize_traffic(self, probability, max_value):
         for edge in self.edges:
             vertex = self.vertices.get(edge.start_vertex)
-            vertex.initialize_traffic(probability, max_value, weight)
+            vertex.initialize_traffic(probability, max_value, edge.weight)
 
     def save_results(self, all_steps_gdf: gpd.GeoDataFrame):
         results_folder = "Results"
@@ -210,19 +275,21 @@ class Graph:
             # plt.show()
         pd.set_option('display.max_colwidth', None)
         pd.set_option('display.max_columns', None)
-        print(gdf)
         return gdf
+
 
 def get_color(traffic_value, max_traffic, cmap):
     normalized_value = traffic_value / max_traffic
-    print(mcolors.to_hex(cmap(normalized_value)))
     return mcolors.to_hex(cmap(normalized_value))
+
 
 def get_color2(traffic_value, max_traffic):
     # Normalizacja wartości 'traffic' do zakresu 0-1
     normalized_value = traffic_value / max_traffic
     # Interpolacja kolorów (zielony -> żółty -> czerwony)
-    return mcolors.to_hex(mcolors.rgb_to_hsv(mcolors.to_rgb('green')) * (1 - normalized_value) + mcolors.rgb_to_hsv(mcolors.to_rgb('red')) * normalized_value)
+    return mcolors.to_hex(mcolors.rgb_to_hsv(mcolors.to_rgb('green')) * (1 - normalized_value) + mcolors.rgb_to_hsv(
+        mcolors.to_rgb('red')) * normalized_value)
+
 
 colors = ["green", "yellow", "red"]
 n_bins = 100  # Liczba kroków w gradiencie
@@ -232,32 +299,291 @@ rows = Data_Processing.get_cracow_graph()
 graph = Graph()
 
 for row in rows:
-    start_point, end_point, source, target, cost, highway, weight = row
-    start_point = loads(start_point)
-    end_point = loads(end_point)
-    graph.add_edge(start_point, end_point, source, target, weight)
+    # start_point, end_point, source, target, cost, highway, oneway, weight = row
+    # start_point = loads(start_point)
+    # end_point = loads(end_point)
+    start_point, end_point, source, target, highway, oneway, weight = row
+    graph.add_edge(start_point, end_point, source, target, oneway, weight)
 
-graph.initialize_traffic(probability=0.5, max_value=100)
-geo_df = graph.run_pregel(max_supersteps=1)
+print(f"Liczba wierzchołków: {len(graph.vertices)}")
+print(f"Liczba krawędzi: {len(graph.edges)}")
+
+steps = 50
+
+graph.initialize_traffic(probability=0.75, max_value=90)
+gdf = graph.run_pregel(max_supersteps=steps)
+gdf = process_dataframe(gdf)
+
+# gdf = gdf.head(600)
+
+print(gdf)
+
+for i in range(steps):
+    mapa = folium.Map(location=[50.074, 19.92], zoom_start=14)
+
+    for _, row in gdf.iterrows():
+        folium.PolyLine(
+            locations=[(y, x) for x, y in row['geometry'].coords],
+            color=get_color(row['traffic'][i], 60, cmap),
+            weight=2
+        ).add_to(mapa)
+
+    mapa.save(f'Traffic_{i}.html')
+    import webbrowser
+
+    webbrowser.open(f'Traffic_{i}.html')
+
+# # Function to extract coordinates from LineStrings
+# def linestring_to_coords(linestring):
+#     return [[x, y] for x, y in linestring.coords]
+#
+# # Prepare data for plotting
+# gdf['coords'] = gdf['geometry'].apply(linestring_to_coords)
+# max_length = gdf['traffic'].apply(len).max()
+#
+# # Create a Plotly figure
+# fig = go.Figure()
+#
+# # Add streets as lines
+# for _, row in gdf.iterrows():
+#     fig.add_trace(go.Scattermapbox(
+#         lon = [point[0] for point in row['coords']],
+#         lat = [point[1] for point in row['coords']],
+#         mode = 'lines',
+#         line = dict(width = 2, color = 'green'), # Initial color
+#         name = '',
+#     ))
+#
+# # Update layout
+# fig.update_layout(
+#     mapbox_style="open-street-map",
+#     mapbox_zoom=13,
+#     mapbox_center = {"lat": 50.09151, "lon": 19.93074}
+# )
+#
+# # Create animation frames
+# frames = []
+# for i in range(max_length):
+#     frame_data = []
+#     for _, row in gdf.iterrows():
+#         traffic_value = row['traffic'][i] if i < len(row['traffic']) else row['traffic'][-1]
+#         color = 'red' if traffic_value > 30 else 'green'
+#         frame_data.append(go.Scattermapbox(
+#             lon = [point[0] for point in row['coords']],
+#             lat = [point[1] for point in row['coords']],
+#             mode = 'lines',
+#             line = dict(width = 2, color = color),
+#             name = '',
+#         ))
+#     frames.append(go.Frame(data=frame_data, name=str(i)))
+#
+# # Add frames to the figure
+# fig.frames = frames
+#
+# # Add a slider to control animation
+# sliders = [{
+#     'steps': [{'method': 'animate', 'label': str(k), 'args': [[str(k)], {'frame': {'duration': 1000, 'redraw': True}}]} for k in range(max_length)]
+# }]
+#
+# fig.update_layout(sliders=sliders)
+#
+# # Show the figure
+# fig.show()
 
 
-mapa = folium.Map(location=[50.08, 19.94], zoom_start=12)
+#
+# # Define the color map
+# def traffic_color(value):
+#     if value <= 50:
+#         return 'green'
+#     elif 50 < value <= 100:
+#         return 'yellow'
+#     else:
+#         return 'red'
+#
+# # Initialize the plot
+# fig, ax = plt.subplots()
+#
+# def animate(i):
+#     ax.clear()
+#     ax.set_xlim(19.929, 19.932)
+#     ax.set_ylim(50.091, 50.092)
+#     for index, row in gdf.iterrows():
+#         # Determine the color based on the traffic value
+#         color = traffic_color(row['traffic'][i])
+#         ax.plot(*row['geometry'].xy, color=color, linewidth=2)
+#
+#     ax.set_title(f"Traffic at second {i+1}")
+#
+#
+# from matplotlib.animation import FuncAnimation
+# # Creating the animation
+# ani = FuncAnimation(fig, animate, frames=3, interval=1000, repeat=False)
+#
+# from matplotlib.animation import writers
+# print(writers.list())
+#
+# # Save the animation as a GIF file
+# ani.save('traffic_animation.gif', writer='pillow', fps=1)
 
-# Dodaj linie na mapie
-for _, row in geo_df.iterrows():
-    # Użyj folium.PolyLine, aby dodać linie na mapie
-    # Kolor linii jest zależny od wartości 'traffic'
-    # print(get_color2(row['traffic'][0], 200))
-    folium.PolyLine(
-        locations=[(y, x) for x, y in row['geometry'].coords],
-        color=get_color(row['traffic'][0], 20, cmap), # Zmień na odpowiednią funkcję kolorów w zależności od 'traffic'
-        weight=2
-    ).add_to(mapa)
+# # Prepare the data
+# def extract_coordinates(linestring):
+#     return list(linestring.coords)
+#
+# gdf['coordinates'] = gdf['geometry'].apply(extract_coordinates)
+#
+# # Function to get color based on traffic value
+# def get_color(traffic_value):
+#     if traffic_value > 100:
+#         return 'red'
+#     else:
+#         return 'green'
+#
+# # Initialize the figure
+# fig = go.Figure()
+#
+# # Add the initial state of the lines (using the first traffic value)
+# for idx, row in gdf.iterrows():
+#     fig.add_trace(go.Scattermapbox(
+#         lon=[p[0] for p in row['coordinates']],
+#         lat=[p[1] for p in row['coordinates']],
+#         mode='lines',
+#         line=dict(color=get_color(row['traffic'][0])),
+#         hoverinfo='none'
+#     ))
+#
+# # Set up the map layout
+# fig.update_layout(
+#     mapbox_style="open-street-map",
+#     mapbox_center={"lat": 50.06143, "lon": 19.93658},
+#     mapbox_zoom=12,
+#     showlegend=False,
+# )
+#
+# # Add animation frames
+# frames = []
+# for i in range(len(gdf['traffic'].iloc[0])):
+#     frame_data = []
+#     for idx, row in gdf.iterrows():
+#         frame_data.append(go.Scattermapbox(
+#             lon=[p[0] for p in row['coordinates']],
+#             lat=[p[1] for p in row['coordinates']],
+#             mode='lines',
+#             line=dict(color=get_color(row['traffic'][i]))
+#         ))
+#     frames.append(go.Frame(data=frame_data))
+#
+# fig.frames = frames
+#
+# # Add a slider to control animation
+# fig.update_layout(
+#     updatemenus=[dict(
+#         type="buttons",
+#         buttons=[dict(label="Play",
+#                       method="animate",
+#                       args=[None, {"frame": {"duration": 1000, "redraw": True}}])])]
+# )
+#
+# # Show the figure
+# fig.show()
 
-# Wyświetl mapę
-mapa.save('run_map.html')
-import webbrowser
-webbrowser.open('run_map.html')
+
+#
+# line_data = []
+# for _, row in gdf.iterrows():
+#     line = row['geometry'].coords[:]
+#     lon, lat = zip(*line)  # Rozpakowanie współrzędnych do osobnych list
+#     line_data.append({'type': 'LineString', 'traffic': row['traffic'][0], 'coordinates': list(zip(lon, lat))})
+#
+# # Tworzymy pusty obiekt figury
+# fig = go.Figure()
+#
+# # Dodajemy ścieżki dla każdego segmentu drogi
+# for segment in line_data:
+#     fig.add_trace(
+#         go.Scattergeo(
+#             lon=[coord[0] for coord in segment['coordinates']],
+#             lat=[coord[1] for coord in segment['coordinates']],
+#             mode='lines',
+#             line=dict(width=2, color=get_color(segment['traffic'], 50, cmap)),  # Tutaj możesz dostosować grubość linii
+#         )
+#     )
+#
+# # Ustawienie layoutu mapy
+# fig.update_layout(
+#     title='Mapa korków w Krakowie',
+#     geo=dict(
+#         resolution=50,
+#         showland=True,
+#         landcolor="rgb(217, 217, 217)",
+#         subunitcolor="rgb(255, 255, 255)",
+#         countrycolor="rgb(255, 255, 255)",
+#         showlakes=True,
+#         lakecolor="rgb(255, 255, 255)",
+#         showsubunits=True,
+#         showcountries=True,
+#         showocean=True,
+#         oceancolor="rgb(255, 255, 255)",
+#         projection_type="mercator",
+#         center=dict(lon=19.9368, lat=50.0619),  # Centrum Krakowa
+#         lonaxis_range=[19.8, 20.1],  # Zakres współrzędnych geograficznych
+#         lataxis_range=[49.9, 50.1]
+#     ),
+#     showlegend=False,
+#     mapbox_style="carto-positron",
+#     mapbox_zoom=14,  # Zwiększ wartość dla większego przybliżenia
+#     mapbox_center={"lat": 50.07, "lon": 19.92},  # Ustawienie na centrum Krakowa
+#     width=1600,  # Szerokość mapy w pikselach
+#     height=600,  # Wysokość mapy w pikselach
+# )
+#
+# fig.show()
+#
 
 
-
+# # gdf = gpd.GeoDataFrame(df, geometry=gpd.GeoSeries.from_wkt(df['geometry']))
+#
+# # Ustawienie układu współrzędnych
+# gdf.set_crs(epsg=4326, inplace=True)
+# # gdf['traffic2'] = gdf['traffic'].get(0)
+# gdf['traffic2'] = gdf['traffic'].apply(lambda x: x[0])
+# gdf = gdf.head(500)
+#
+# fig = go.Figure()
+#
+# # Iterujemy przez każdy wiersz DataFrame
+# for _, row in gdf.iterrows():
+#     # Parsujemy geometrię linestring do listy współrzędnych
+#     line = row['geometry'].coords[:]
+#     lat, lon = zip(*line)
+#
+#     # Dodajemy ślad dla każdego segmentu drogi
+#     fig.add_trace(
+#         go.Scattermapbox(
+#             lat=lat,
+#             lon=lon,
+#             mode='lines',  # Ustawiamy tryb na linie
+#             line=dict(width=2, color='red'),  # Tutaj można dostosować na podstawie 'traffic'
+#         )
+#     )
+#
+# # Ustawienie widoku mapy i stylu
+# fig.update_layout(
+#     mapbox_style="carto-positron",
+#     mapbox_zoom=12,
+#     mapbox_center={"lat": 50.0619, "lon": 19.9368}
+# )
+#
+# # Wyświetlenie figury
+# fig.show()
+#
+# # Tworzenie mapy
+# # fig = px.line_mapbox(gdf, lat=gdf.geometry.centroid.y, lon=gdf.geometry.centroid.x,
+# #                      color='traffic2', zoom=12, height=600)
+# #
+# # # Ustawienie tokena Mapbox (jeśli go masz)
+# # # fig.update_layout(mapbox_style="open-street-map") # alternatywa, jeśli nie masz tokena Mapbox
+# # fig.update_layout(mapbox_style="carto-positron", mapbox_zoom=12, mapbox_center = {"lat": 50.0619, "lon": 19.9368})
+# #
+# # fig.show()
+# time.sleep(10000)
---
+++ Data/Data_Processing.py
@@ -0,0 +1,270 @@
+import psycopg2
+from shapely.wkt import loads
+import matplotlib.pyplot as plt
+from shapely.geometry import LineString
+from shapely.ops import transform
+from pyproj import Transformer
+
+HIGHWAY_TO_WEIGHT = {
+    "primary": 20,
+    "secondary": 18,
+    "tertiary": 16,
+    "primary_link": 15,
+    "secondary_link": 14,
+    "tertiary_link": 12,
+    "residential": 6,
+    "service": 3,
+    "living_street": 1
+}
+
+
+def get_weight(highway_type):
+    return HIGHWAY_TO_WEIGHT.get(highway_type, 1)
+
+
+# def draw_edge(ax, start_wkt, end_wkt, source, target, weight):
+#     start = loads(start_wkt)
+#     end = loads(end_wkt)
+#     # print(start.x)
+#     # if start.x < 2217500 or end.x < 2217500 or start.x > 2218500 or start.y > 6458400 or end.y > 6459000:
+#     #     return
+#     ax.plot([start.x, end.x], [start.y, end.y], 'b-', linewidth=(weight/5)+1) # Krawędzie jako niebieskie linie
+#     ax.plot(start.x, start.y, 'ro', markersize=2) # Wierzchołki jako czerwone kropki
+#     # ax.text(start.x, start.y, f'{source}', color='green', fontsize=6)
+#     ax.plot(end.x, end.y, 'ro', markersize=2)
+#     # ax.text(end.x, end.y, f'{target}', color='green', fontsize=6)
+#
+#
+# def get_cracow_graph():
+#     try:
+#         conn = psycopg2.connect(
+#             host="localhost",
+#             port="5432",
+#             dbname="Cracow_Map",
+#             user="postgres",
+#             password="admin"
+#         )
+#
+#         # Zapytanie SQL do pobrania danych grafu
+#         sql = "SELECT ST_AsText(ST_Transform(ST_StartPoint(way), 4326)) AS start_point, " \
+#               "ST_AsText(ST_Transform(ST_EndPoint(way), 4326)) AS end_point, " \
+#               "source, target, cost, highway " \
+#               "FROM planet_osm_line " \
+#               "WHERE railway IS NULL " \
+#               "AND cost < 1000 " \
+#               "AND highway NOT IN ('footway', 'cycleway', 'elevator', 'steps', 'service');"
+#
+#         # Wykonanie zapytania
+#         cur = conn.cursor()
+#         cur.execute(sql)
+#         rows = cur.fetchall()
+#
+#         # Zamknięcie połączenia
+#         cur.close()
+#         conn.close()
+#
+#         new_rows = []
+#
+#         for row in rows:
+#             weight = get_weight(row[5])
+#             new_rows.append(row + (weight,))
+#
+#         return new_rows
+#
+#     except psycopg2.Error as e:
+#         print(f"Error: Unable to connect to the database - {e}")
+#
+#
+# # get_cracow_graph()
+#
+# def visualize_graph():
+#     rows = get_cracow_graph()
+#
+#     # Tworzenie wykresu
+#     fig, ax = plt.subplots(figsize=(60, 36))  # Ustawienie większego rozmiaru wykresu
+#
+#     # Rysowanie krawędzi
+#     for start, end, source, target, cost, highway, weight in rows:
+#         draw_edge(ax, start, end, source, target, weight)
+#
+#     # Ustawienia wykresu
+#     ax.set_title("Graf Drogowy Krakowa")
+#     ax.set_xlabel("Długość geograficzna")
+#     ax.set_ylabel("Szerokość geograficzna")
+#     plt.show()
+# #
+# #
+# # visualize_graph()
+
+
+
+
+
+
+
+
+
+
+
+
+def draw_edge(ax, start_wkt, end_wkt, source, target, weight):
+    start = start_wkt
+    end = end_wkt
+    # print(start.x)
+    # if start.x < 2217500 or end.x < 2217500 or start.x > 2218500 or start.y > 6458400 or end.y > 6459000:
+    #     return
+    ax.plot([start[0], end[0]], [start[1], end[1]], 'b-', linewidth=(weight/5)+1) # Krawędzie jako niebieskie linie
+    ax.plot(start[0], start[1], 'ro', markersize=2) # Wierzchołki jako czerwone kropki
+    # ax.text(start.x, start.y, f'{source}', color='green', fontsize=6)
+    ax.plot(end[0], end[1], 'ro', markersize=2)
+    # ax.text(end.x, end.y, f'{target}', color='green', fontsize=6)
+
+
+
+
+def get_cracow_graph():
+    try:
+        # conn = psycopg2.connect(
+        #     host="localhost",
+        #     port="5432",
+        #     dbname="Cracow_Map",
+        #     user="postgres",
+        #     password="admin"
+        # )
+
+        conn = psycopg2.connect(
+            host="localhost",
+            port="5432",
+            dbname="Cracow_Map",
+            user="postgres",
+            password="admin"
+        )
+
+        # 163294
+
+        # Zapytanie SQL do pobrania danych grafu
+        sql = "SELECT ST_AsText(ST_Transform(ST_Multi(way), 4326)) AS multiline, " \
+              "source, target, highway, oneway " \
+              "FROM planet_osm_line " \
+              "WHERE railway IS NULL " \
+              "AND cost < 1000 " \
+              "AND highway NOT IN ('footway', 'cycleway', 'elevator', 'steps', 'service', 'path');"
+
+        # sql = "SELECT ST_AsText(ST_Transform(ST_StartPoint(way), 4326)) AS start_point, " \
+        #       "ST_AsText(ST_Transform(ST_EndPoint(way), 4326)) AS end_point, " \
+        #       "source, target, cost, highway " \
+        #       "FROM planet_osm_line_segmented " \
+        #       "WHERE cost < 1000 " \
+        #       "AND highway NOT IN ('footway', 'cycleway', 'elevator', 'steps', 'service');"
+
+        # Wykonanie zapytania
+        cur = conn.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+
+        # Zamknięcie połączenia
+        cur.close()
+        conn.close()
+
+        # print(rows)
+
+        id = 0
+        new_rows = []
+
+        transformer = Transformer.from_crs("EPSG:4326", "EPSG:32633", always_xy=True)
+
+        # Funkcja do transformacji punktów
+        def transform_point(x, y):
+            return transformer.transform(x, y)
+
+        # Transformacja LineString do UTM
+
+
+
+        for row in rows:
+            multilinestring = row[0]
+            source = row[1]
+            target = row[2]
+            highway = row[3]
+            oneway = row[4]
+            multi_line = loads(multilinestring)
+            for line in multi_line.geoms:
+                meters = transform(transform_point, line).length
+                line_length = len(line.coords)
+                if meters > 200:
+                    number_of_segments = int(meters // 100)
+                    step = int(line_length//number_of_segments)
+                    if step == 0:
+                        step = 1
+                    # print()
+                    # print("values")
+                    # print(f"line_length:{line_length}")
+                    # print(f"step:{step}")
+                    # print()
+                    for i in range(0, line_length - 1, step):
+                        start_point = line.coords[i]
+                        last_index = line_length - 1 if step + i >= line_length - 1 else i + step
+                        # print(f"i:{i}")
+                        # print(f"last_index:{last_index}")
+                        end_point = line.coords[last_index]
+                        if i == 0:
+                            new_source = str(source)
+                        else:
+                            new_source = f"custom_{id}"
+                            id += 1
+                        if last_index == line_length - 1:
+                            new_target = str(target)
+                        else:
+                            new_target = f"custom_{id}"
+                        start_point = [start_point[0], start_point[1]]
+                        end_point = [end_point[0], end_point[1]]
+                        new_rows.append([start_point, end_point, new_source, new_target, highway, oneway, get_weight(highway)])
+                else:
+                    start_point = line.coords[0]
+                    end_point = line.coords[-1]
+                    start_point = [start_point[0], start_point[1]]
+                    end_point = [end_point[0], end_point[1]]
+                    new_rows.append([start_point, end_point, source, target, highway, oneway, get_weight(highway)])
+
+                # for i in range(len(line.coords) - 1):
+                #     start_point = line.coords[i]
+                #     end_point = line.coords[i+1]
+                #     if i == 0:
+                #         new_source = str(source)
+                #     else:
+                #         new_source = f"custom_{id}"
+                #         id += 1
+                #     if i == len(line.coords) - 2:
+                #         new_target = str(target)
+                #     else:
+                #         new_target = f"custom_{id}"
+                #     new_rows.append([start_point, end_point, new_source, new_target, highway, get_weight(highway)])
+        for row in new_rows:
+            print(row)
+
+        return new_rows
+
+    except psycopg2.Error as e:
+        print(f"Error: Unable to connect to the database - {e}")
+
+
+# get_cracow_graph()
+
+def visualize_graph():
+    rows = get_cracow_graph()
+
+    # Tworzenie wykresu
+    fig, ax = plt.subplots(figsize=(60, 36))  # Ustawienie większego rozmiaru wykresu
+
+    # Rysowanie krawędzi
+    for start, end, source, target, highway, weight in rows:
+        draw_edge(ax, start, end, source, target, weight)
+
+    # Ustawienia wykresu
+    ax.set_title("Graf Drogowy Krakowa")
+    ax.set_xlabel("Długość geograficzna")
+    ax.set_ylabel("Szerokość geograficzna")
+    plt.show()
+
+
+# visualize_graph()
